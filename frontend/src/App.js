bd3ee5b893c499cfebf2da5e6cd1a33a8e8cf175
darvid7 <bvdarvid@gmail.com>
2017-08-05 16:37:00 +1000

    Init

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import { FullnessMap } from './FullnessMap';
+import { Train } from './Train';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Train/>
+      </div>
+    );
+  }
+}
+
+export default App;

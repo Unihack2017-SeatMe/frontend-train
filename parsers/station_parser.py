3bc9896ce08823a8c031c19411bdaccde0a84198
darvid7 <bvdarvid@gmail.com>
2017-08-05 21:20:41 +1000

    Add auto complete search bar

--- parsers/station_parser.py
+++ parsers/station_parser.py
@@ -1,9 +1,36 @@
-with open('stations.txt', 'r') as fh:
-	stations = []
-	data = fh.readlines()
-	for station in data:
-		station = station.strip()
-		stations.append(station)
-	with open('station.array', 'w') as f:
+import re
 
-		f.write(repr(str(stations).replace("\'", '\"')))
+RE_STATION_MATCH = '\">(\w+\s\w+|\w+|\w+\s\w+\s\w+)</a>'
+
+s = set()
+
+with open('all_train_stations.html', 'r') as fh:
+	stations = []
+	html_data = fh.read()
+	print(html_data)
+	station_matches = re.findall(RE_STATION_MATCH, html_data)
+
+	print(station_matches) 
+	print(len(station_matches))
+	for station in station_matches:
+		station = station.strip()
+		if station in s:
+			print('duplicate station: ' + str(station))
+			continue
+		s.add(station)
+		station_dict = {"name": station}
+		stations.append(station_dict)
+	print('finished parsing')
+	print("unique stations: " + str(len(s)))
+	stations.sort(key = lambda d:d['name'])
+	with open('station.json', 'w') as f:
+		f.write("[\n")
+		for i in range(len(stations)):
+			station_dict = stations[i]
+			if i == len(stations) -1:
+				f.write("\t" + str(station_dict).replace("\'", '\"') + "\n")
+			else:
+				f.write("\t" + str(station_dict).replace("\'", '\"') + ",\n")
+		f.write("]")
+		# f.write(str(stations).replace("\'", '\"'))
+		print('finished writing')
